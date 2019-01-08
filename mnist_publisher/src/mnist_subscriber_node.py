874e63e8c509b312232f0de69b64b8131a97f810
avnishnarayan <avnishn@uw.edu>
2019-01-08 08:12:49 -0800

    add trajectory displayer markers to display trajectory in rviz

--- mnist_publisher/src/mnist_subscriber_node.py
+++ mnist_publisher/src/mnist_subscriber_node.py
@@ -3,6 +3,9 @@
 import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Point
+
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
 import tensorflow as tf
@@ -13,42 +16,6 @@ import numpy
 import math
 import sys
 
-
-# converts image to cv2 format and then an image classifier is run on it
-class image_converter:
-  def __init__(self):
-
-    self.bridge = CvBridge()
-    self.image_sub = rospy.Subscriber("mnist_image",Image,self.callback)
-    self.graph = tf.Graph()
-    self.sess = tf.Session(graph=self.graph)
-    model_path = "./src/mnist_trained_network/model"
-    tf.saved_model.loader.load(self.sess,
-                              [tag_constants.SERVING],
-                              model_path
-                              )
-    # input tensor, x
-    pred2 = self.graph.get_tensor_by_name("out:0")
-    # output tensor, pred
-    x = self.graph.get_tensor_by_name('x:0')
-    self.tensors = (pred2, x)
-
-  # rosnode callback, accepts, converts, classifies image, and finding image contours
-  def callback(self,data):
-    try:
-      cv_image = self.bridge.imgmsg_to_cv2(data, "mono8")
-    except CvBridgeError as e:
-      print(e)
-    resized_image = cv2.resize(cv_image, (28, 28))
-    classification = self.sess.run(tf.argmax(self.tensors[0], 1), \
-                                        feed_dict={self.tensors[1]:\
-                                        [numpy.asarray(resized_image).ravel()]})
-    _, contours, hierarchy = cv2.findContours(cv_image.copy(), \
-      cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    
-    cv2.drawContours(cv_image, contours, -1, (255,255,0), 3)
-    cv2.imshow("Image window", cv_image)
-    cv2.waitKey(100)
 
 class DynamicsAndKinematics(object):
   
@@ -77,18 +44,96 @@ class DynamicsAndKinematics(object):
     x = [curr_x]
     y = [curr_y]
 
-    while( not ( (end_point[1]-0.01 <= curr_y <= end_point[1] + 0.01) and \
-                  (end_point[0]-0.01 <= curr_x <= end_point[0] + 0.01)) ):
-      curr_y = a * end_point[1] + (1-a) * curr_y
-      curr_x = a * end_point[0] + (1-a) * curr_x
+    while( not ( (end_angles[1]-0.01 <= curr_y <= end_angles[1] + 0.01) and \
+                  (end_angles[0]-0.01 <= curr_x <= end_angles[0] + 0.01)) ):
+      curr_y = a * end_angles[1] + (1-a) * curr_y
+      curr_x = a * end_angles[0] + (1-a) * curr_x
       print(curr_x, curr_y)
       x.append(curr_x)
       y.append(curr_y)
     return x, y
 
+class trajectoryDisplayer(object):
+  def __init__(self):
+    self.markerPub = rospy.Publisher('robotMarker', Marker, queue_size=10)
+    self.marker = Marker()
+    self.marker.header.frame_id = "/my_fixed_frame"
+    self.marker.type = self.marker.LINE_STRIP
+    self.marker.action = self.marker.ADD
+
+    self.marker.scale.x = 0.1
+    self.marker.scale.y = 0.1
+    self.marker.scale.z = 0.1
+
+    self.marker.color.a = 1.0
+    self.marker.color.r = 1.0
+    self.marker.color.g = 1.0
+    self.marker.color.b = 0.0
+
+     # marker orientaiton
+    self.marker.pose.orientation.x = 0.0
+    self.marker.pose.orientation.y = 0.0
+    self.marker.pose.orientation.z = 0.0
+    self.marker.pose.orientation.w = 1.0
+
+    # marker position
+    self.marker.pose.position.x = 0.0
+    self.marker.pose.position.y = 0.0
+    self.marker.pose.position.z = 0.0
+
+    self.marker.points = []
+  def addContourPointsAndPublish(self, contours):
+    for contour in contours:
+
+  # clear all of the previous markers
+  def clearPrevious(self):
+    self.marker.action = self.marker.DELETEALL
+    self.marker.points = []
+    self.markerPub.publish(self.marker)
+
+# converts image to cv2 format and then an image classifier is run on it
+class image_converter:
+  def __init__(self):
+    # currently used for displaying trajectory
+    self.ik = DynamicsAndKinematics(0.8,0.8)
+
+    self.bridge = CvBridge()
+    self.image_sub = rospy.Subscriber("mnist_image",Image,self.callback)
+    self.graph = tf.Graph()
+    self.sess = tf.Session(graph=self.graph)
+    model_path = "./src/mnist_trained_network/model"
+    tf.saved_model.loader.load(self.sess,
+                              [tag_constants.SERVING],
+                              model_path
+                              )
+    # input tensor, x
+    pred2 = self.graph.get_tensor_by_name("out:0")
+    # output tensor, pred
+    x = self.graph.get_tensor_by_name('x:0')
+    self.tensors = (pred2, x)
+
+  # rosnode callback, accepts, converts, classifies image, and finding image contours
+  def callback(self,data):
+    try:
+      cv_image = self.bridge.imgmsg_to_cv2(data, "mono8")
+    except CvBridgeError as e:
+      print(e)
+    resized_image = cv2.resize(cv_image, (28, 28))
+    classification = self.sess.run(tf.argmax(self.tensors[0], 1), \
+                                        feed_dict={self.tensors[1]:\
+                                        [numpy.asarray(resized_image).ravel()]})
+    _, contours, hierarchy = cv2.findContours(cv_image.copy(), \
+      cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(cv_image, contours, -1, (255,255,0), 3)
+    cv2.imshow("Image window", cv_image)
+    cv2.waitKey(100)
+
+
+
 if __name__ == '__main__':
   ic = image_converter()
   rospy.init_node('mnist_image_subscriber', anonymous=False)
+  rospy.Rate(50)
   try:
     rospy.spin()
   except KeyboardInterrupt:
