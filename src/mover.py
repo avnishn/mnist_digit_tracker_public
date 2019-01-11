e94c67fe5fd0ce384ec19b9b78a6079502c38f1c
avnish <avnishna@usc.edu>
2019-01-10 19:47:48 -0800

    fix naming of joints in .xacro file and in mover.py to allow for joint_State messages to be published

--- src/mover.py
+++ src/mover.py
@@ -12,7 +12,7 @@ def talker():
     hello_str = JointState()
     hello_str.header = Header()
     hello_str.header.stamp = rospy.Time.now()
-    hello_str.name = ['base_link__link_2', 'link_2__link_3']
+    hello_str.name = ['joint0', 'joint1']
     hello_str.position = [1.575, 1.575]
     hello_str.velocity = []
     hello_str.effort = []
